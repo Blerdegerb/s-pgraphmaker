cbd2617033995a8ea8dc80c9487fda2a04679d86
Scott Shannon <scottshannon@mail.adelphi.edu>
2021-10-06 23:34:27 -0400

    testing git

---
+++ src/Calculation.java
@@ -0,0 +1,12 @@
+
+public class Calculation {
+	/**
+	 * Calculates the mean of an array of doubles.
+	 * @param data
+	 * @return The mean.
+	 */
+	public static double calcMean(double[] data) {
+		//TODO: actually implement
+		return data[0];
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		double[] testArray = {5,10,15,2.1};
+		
+		double testArrayMean = Calculation.calcMean(testArray);
+		
+		System.out.println(testArrayMean);
+	}
+
+}
